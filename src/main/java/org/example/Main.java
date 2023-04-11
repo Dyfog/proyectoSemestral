fc5f0a7ffbc9273b0d83cd72b1c6e61de7203c05
Dyfog <111368031+Dyfog@users.noreply.github.com>
2023-04-11 17:33:44 -0400

    merge brach benjamin

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        eleccionMaterial();
+        eleccionLargo();
     }
     public static int eleccionMaterial(){
         System.out.println("Ingrese el número que corresponda a su elección: ");
@@ -13,6 +15,7 @@ public class Main {
         int eleccion=ingresarSoloNumero();
         return eleccion;
     }
+
     public static int calculoMaterial(int eleccion){
         switch (eleccion){
             case 0:
@@ -27,8 +30,9 @@ public class Main {
         }
         return 0;
     }
+
     public static int[] eleccionDiseño() {
-        int elecciones[] =new int[2];
+        int[] elecciones =new int[2];
         System.out.println("Ingrese cantidad de diseños, son 1 por cada uña, 10 en total");
         System.out.println("Diseños simples: ");
         int cantSimples = ingresarSoloNumero();
@@ -45,10 +49,12 @@ public class Main {
         elecciones[1]= cantSofisticados;
         return elecciones;
     }
+
     public static int calculoDiseño(int[] eleccion) {
-        int costo = ((eleccion[0] * 500) + eleccion[1] * 1200);
+        int costo = ((eleccion[0] * 500) + (eleccion[1] * 1200));
         return costo;
     }
+
     public static boolean validarRangoNumero(int numero, int min, int max) {
         if (numero < min || numero > max) {
             System.out.println("El numero no esta dentro de los parametros");
@@ -88,13 +94,15 @@ public class Main {
         int numero = in.nextInt();
         return numero;
     }
+
     public static int eleccionLargo(){
         System.out.println("ingrese el largo de uña que desea");
         System.out.println("0.- Natural");
         System.out.println("1.- S");
         System.out.println("2.- M");
         System.out.println("3.- L");
-        return ingresarSoloNumero();
+        int eleccion = ingresarSoloNumero();
+        return eleccion;
     }
 
    /*public static int calcValorLargo(int eleccionLargo){
@@ -113,4 +121,58 @@ public class Main {
         }
    }
     */
+
+    public static int calcularIva(int precioPreliminar) {
+        int iva = 0;
+        iva = (precioPreliminar * 19) / 100;
+        return iva;
+    }
+
+    public static int calcularPrecioFinal(int[] precios) {
+        int sumas = 0;
+        for (int i = 0; i < precios.length; i++) {
+            sumas += precios[i];
+        }
+        return sumas;
+    }
+
+    public static int[] valorDecora(int[] arregloAux) {
+        int[] arregloAux2 = new int[2];
+        arregloAux2[0] = arregloAux[0] * 600;
+        arregloAux2[1] = arregloAux[1] * 1500;
+        return arregloAux2;
+    }
+
+    public static int[] eleccionDecora() {
+        Scanner teclado = new Scanner(System.in);
+        int[] arregloAux = new int[2];
+        int aux = 0;
+        int aux2 = 0;
+        do {
+            aux = teclado.nextInt();
+        } while (aux > 10);
+        arregloAux[0] = aux;
+        do {
+            aux2 = teclado.nextInt();
+        } while (aux2 > (10 - aux));
+        arregloAux[1] = aux2;
+        return arregloAux;
+    }
+
+    public static int valorColores(int aux) {
+        if (aux == 1 || aux == 2) {
+            return 8000;
+        } else if (aux > 2) {
+            return 9000;
+        }
+        return 0;
+    }
+
+    public static int eleccionColores() {
+        Scanner teclado = new Scanner(System.in);
+        int aux = 0;
+        aux = teclado.nextInt();
+        return aux;
+    }
+
 }
